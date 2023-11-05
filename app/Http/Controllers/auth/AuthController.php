313084c5e4d34157e498eeb31803c79273be049a
João Silva <2211429@my.ipleiria.pt>
2023-11-05 19:38:55 +0000

    Add vcard creation and confirmation code validation to login endpoint

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -3,13 +3,36 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreVcardRequest;
+use App\Models\Vcard;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        //create vcard if it does not exist
+        if (!Vcard::where('phone_number', $request->username)-> exists()) {
+            $vcard = new Vcard();
+            $vcard->phone_number = $request->username;
+            $vcard->password = bcrypt($request->password);
+            $vcard->confirmation_code = bcrypt($request->confirmation_code);
+            $vcard->name = $request->username;
+            $vcard->blocked = false;
+            $vcard->email = bin2hex(openssl_random_pseudo_bytes(16)) . '@' . bin2hex(openssl_random_pseudo_bytes(16)) . '.com';
+            $vcard->save();
+        }
+
+        //if confirmation code is not the same then return error
+        if ($request->confirmation_code != Vcard::where('phone_number', $request->username)->first()->confirmation_code) {
+            return response()->json(
+                ['msg' => 'Confirmation code is invalid'],
+                401
+            );
+        }
+
         $passportData = [
             'grant_type' => 'password',
             'client_id' => env('PASSPORT_PASSWORD_GRANT_ID'),
