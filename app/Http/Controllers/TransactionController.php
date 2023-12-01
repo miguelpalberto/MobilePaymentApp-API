722c01f6aada8ea66b012d44e8c344d702db3f6b
João Silva <2211429@my.ipleiria.pt>
2023-12-01 17:56:37 +0000

    Add transaction post endpoint

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\TransactionResource;
+use App\Http\Requests\TransactionRequest;
 use App\Models\Vcard;
+use Illuminate\Support\Facades\DB;
+use App\Http\Resources\TransactionResource;
+use App\Models\Transaction;
+use DateTime;
+use Illuminate\Support\Facades\Hash;
 
 class TransactionController extends Controller
 {
@@ -20,10 +25,78 @@ class TransactionController extends Controller
         if ($count > 0) {
             $transaction = new TransactionResource($vcard->transactions()->orderBy('date', 'desc')->first());
         }
-        
+
         return response()->json([
             'latestTransaction' => $transaction,
             'total' => $count
         ], 200);
+    }
+
+    public function store(TransactionRequest $request)
+    {
+        $validRequest = $request->validated();
+        $vcard = VCard::find($validRequest['vcard']);
+
+        if (!Hash::check($validRequest['confirmation_code'], $vcard->confirmation_code)) {
+            return response()->json([
+                'errors' => [
+                    'confirmation_code' => [
+                        'The confirmation code is incorrect'
+                    ]
+                ]
+            ], 422);
+        }
+
+        if ($vcard->balance < $validRequest['value']) {
+            return response()->json([
+                'errors' => [
+                    'value' => [
+                        'Sorry, you do not have enough balance to make this transaction'
+                    ]
+                ]
+            ], 422);
+        }
+
+        $transaction = DB::transaction(function () use ($validRequest, $vcard) {
+            $utcDatetimeNow = new DateTime('now', new \DateTimeZone('UTC'));
+
+            $transaction = new Transaction();
+            $transaction->vcard = $vcard->phone_number;
+            $transaction->date = $utcDatetimeNow->format('Y-m-d');
+            $transaction->datetime = $utcDatetimeNow;
+            $transaction->type = 'D';
+            $transaction->value = $validRequest['value'];
+            $transaction->payment_type = 'VCARD';
+            $transaction->payment_reference = $validRequest['pair_vcard'];
+            $transaction->old_balance = $vcard->balance;
+            $transaction->new_balance = $vcard->balance - $validRequest['value'];
+            $transaction->pair_vcard = $validRequest['pair_vcard'];
+            $transaction->save();
+
+            $pairVCard = VCard::find($validRequest['pair_vcard']);
+
+            $pairTransaction = new Transaction();
+            $pairTransaction->vcard = $validRequest['pair_vcard'];
+            $pairTransaction->pair_vcard = $vcard->phone_number;
+            $pairTransaction->date = $utcDatetimeNow->format('Y-m-d');
+            $pairTransaction->datetime = $utcDatetimeNow;
+            $pairTransaction->type = 'C';
+            $pairTransaction->value = $validRequest['value'];
+            $pairTransaction->payment_type = 'VCARD';
+            $pairTransaction->payment_reference = $vcard->phone_number;
+            $pairTransaction->old_balance = $pairVCard->balance;
+            $pairTransaction->new_balance = $pairVCard->balance - $validRequest['value'];
+            $pairTransaction->save();
+
+            $pairVCard->balance = $pairTransaction->new_balance;
+            $pairVCard->save();
+
+            $vcard->balance = $transaction->new_balance;
+            $vcard->save();
+
+            return $transaction;
+        });
+
+        return $transaction;
     }
 }
---
+++ app/Http/Requests/TransactionRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\PaymentReferenceValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'vcard' => 'required|string|exists:vcards,phone_number,deleted_at,NULL',
+            'confirmation_code' => 'required|string',
+            'value' => 'required|decimal:0,2|min:0.01',
+            'pair_vcard' => 'required|regex:/^9\d{8}$/|string|different:vcard|exists:vcards,phone_number,deleted_at,NULL'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'vcard.required' => 'The vcard field is required',
+            'vcard.string' => 'The vcard field must be a string',
+            'vcard.exists' => 'The vcard does not exist',
+            'confirmation_code.required' => 'The confirmation code field is required',
+            'confirmation_code.string' => 'The confirmation code field must be a string',
+            'value.required' => 'The value field is required',
+            'value.decimal' => 'The value field must be a decimal number',
+            'value.min' => 'The value field must be greater than 0',
+            'pair_vcard.required' => 'The pair vcard field is required',
+            'pair_vcard.regex' => 'The pair vcard field must be a valid phone number',
+            'pair_vcard.string' => 'The pair vcard field must be a string',
+            'pair_vcard.different' => 'The pair vcard field must be different from vcard',
+            'pair_vcard.exists' => 'The pair vcard does not exist'
+        ];
+    }
+}
